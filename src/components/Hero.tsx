11b6f6ffb82b026e431f28ba4b4af50e04788734
Kacper Malek <kacper.malek0828@gmail.com>
2023-07-05 12:36:01 +0200

    adjust styles for mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,18 @@
-import { IconStarFilled, IconStarHalfFilled } from "@tabler/icons-react";
+"use client";
+
+import {
+  IconStarFilled,
+  IconStarHalfFilled,
+  IconMenu2,
+  IconX,
+} from "@tabler/icons-react";
+import { Disclosure } from "@headlessui/react";
 
 import { Container } from "./Container";
 import { Logo } from "./Logo";
 import { Avatar } from "./Avatar";
+import { cn } from "@/lib/cn";
+import { usePathname } from "next/navigation";
 
 const navigation = [
   { name: "Strona Główna", href: "#home" },
@@ -16,66 +26,99 @@ const navigation = [
 ];
 
 export function Hero() {
+  const pathname = usePathname();
+
   return (
-    <div
+    <Disclosure
+      as="div"
       id="home"
-      className="bg-hero h-[90vh] flex flex-col bg-center bg-no-repeat object-cover"
+      className="bg-hero-mobile md:bg-hero h-[95vh] lg:h-[90vh] flex flex-col bg-center lg:bg-center bg-no-repeat object-cover"
     >
-      <div className="p-2 border-b border-gray-600/40">
-        <Container className="flex items-center justify-between font-light text-gray-200">
-          <p>Robotnicza 236, 82-300 Elbląg</p>
-          <a href="tel:506-166-760">Tel: 506-166-760</a>
-        </Container>
-      </div>
-      <div className="p-2">
-        <Container className="flex items-center justify-between">
-          <Logo />
-          <ul className="flex items-center gap-x-5">
-            {navigation.map((item, index) => (
-              <li key={index}>
-                <a href={item.href} className="text-gray-200">
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </Container>
-      </div>
-      <div className="flex flex-col items-center justify-center grow">
-        <div className="w-3/4 mx-auto text-center">
-          <h1 className="font-bold tracking-tighter text-7xl text-lime-500">
-            Osiągnij swoje najlepsze ja!
-          </h1>
-          <p className="w-3/4 mx-auto mt-8 font-light leading-7 tracking-tight text-gray-200">
-            Stawiamy na jakość, innowację i postępy. Oferujemy szeroki wybór
-            zajęć fitness, treningów siłowych i indywidualnego doradztwa
-            treningowego, aby pomóc Ci osiągnąć swoje cele i poczuć się
-            doskonale we własnej skórze.
-          </p>
-        </div>
-
-        <div className="flex items-center mt-16 space-x-4">
-          <div className="flex items-center">
-            <IconStarFilled size={24} className="text-lime-500" />
-            <IconStarFilled size={24} className="text-lime-500" />
-            <IconStarFilled size={24} className="text-lime-500" />
-            <IconStarFilled size={24} className="text-lime-500" />
-            <IconStarHalfFilled size={24} className="text-lime-500" />
+      {({ open }) => (
+        <>
+          <div className="p-2 border-b border-gray-600/40">
+            <Container className="flex items-center justify-between text-sm font-light text-gray-200 md:text-base">
+              <p>Robotnicza 236, 82-300 Elbląg</p>
+              <a href="tel:506-166-760">Tel: 506-166-760</a>
+            </Container>
           </div>
-          <p className="text-lg font-semibold">
-            na <span className="text-lime-500">84</span> recenzji Google
-          </p>
-        </div>
+          <div className="relative">
+            <Container className="flex items-center justify-between p-2">
+              <Logo />
+              <ul className="items-center hidden gap-x-5 lg:flex">
+                {navigation.map((item, index) => (
+                  <li key={index}>
+                    <a href={item.href} className="text-gray-200">
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-200 rounded-md lg:hidden hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lime-500">
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <IconX className="block w-7 h-7" aria-hidden="true" />
+                ) : (
+                  <IconMenu2 className="block w-7 h-7" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </Container>
 
-        <div className="flex items-center justify-center mt-6 space-x-4">
-          <Avatar src="https://lh3.googleusercontent.com/a-/AD_cMMRFEKOqPgHAcaNRXJpSg343A11PRRE7Te0W4UDae5bnGg=w60-h60-p-rp-mo-br100" />
-          <p className="w-1/2 text-lg font-light tracking-tight text-gray-200">
-            &ldquo;Elegancko. Sporo uczęszczających ale nie brakuje sprzętu.
-            Bardzo miła obsługa. osobne salki treningowe. dla mnie ideolo.
-            Polecam i pozdrawiam!&rdquo;
-          </p>
-        </div>
-      </div>
-    </div>
+            <Disclosure.Panel className="absolute w-full border-b-2 lg:hidden bg-zinc-800 border-lime-600">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={cn(
+                      "text-gray-300 hover:bg-gray-700 hover:text-white",
+                      "block rounded-md px-3 py-2 text-base font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel>
+          </div>
+          <div className="flex flex-col items-center justify-center grow">
+            <div className="w-3/4 mx-auto text-center">
+              <h1 className="text-5xl font-bold tracking-tighter lg:text-7xl text-lime-500">
+                Osiągnij swoje najlepsze ja!
+              </h1>
+              <p className="hidden w-full mx-auto mt-8 font-light leading-7 tracking-tight text-gray-200 md:block lg:w-3/4">
+                Stawiamy na jakość, innowację i postępy. Oferujemy szeroki wybór
+                zajęć fitness, treningów siłowych i indywidualnego doradztwa
+                treningowego, aby pomóc Ci osiągnąć swoje cele i poczuć się
+                doskonale we własnej skórze.
+              </p>
+            </div>
+
+            <div className="flex items-center mt-16 space-x-4">
+              <div className="flex items-center">
+                <IconStarFilled size={24} className="text-lime-500" />
+                <IconStarFilled size={24} className="text-lime-500" />
+                <IconStarFilled size={24} className="text-lime-500" />
+                <IconStarFilled size={24} className="text-lime-500" />
+                <IconStarHalfFilled size={24} className="text-lime-500" />
+              </div>
+              <p className="text-lg font-semibold">
+                na <span className="text-lime-500">84</span> recenzji Google
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center mt-6 space-x-4 md:flex-row">
+              <Avatar src="https://lh3.googleusercontent.com/a-/AD_cMMRFEKOqPgHAcaNRXJpSg343A11PRRE7Te0W4UDae5bnGg=w60-h60-p-rp-mo-br100" />
+              <p className="w-3/4 mt-4 text-lg font-light tracking-tight text-gray-200 md:mt-0 lg:w-1/2">
+                &ldquo;Elegancko. Sporo uczęszczających ale nie brakuje sprzętu.
+                Bardzo miła obsługa. osobne salki treningowe. dla mnie ideolo.
+                Polecam i pozdrawiam!&rdquo;
+              </p>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
   );
 }
