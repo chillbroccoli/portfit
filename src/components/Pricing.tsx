48ad405e8e99ef177a8d89fabee441e9c251f637
Kacper Malek <kacper.malek0828@gmail.com>
2023-07-05 11:06:10 +0200

    add Pricing component

---
+++ src/components/Pricing.tsx
@@ -0,0 +1,86 @@
+import { cn } from "@/lib/cn";
+import { IconCircleCheck } from "@tabler/icons-react";
+
+const tiers = [
+  {
+    name: "Wejście Pojedyncze",
+    id: "tier-one-time",
+    href: "#",
+    price: "25zł",
+    features: ["Sala ćwiczeń", "Sala cardio", "Zajęcia grupowe", "Rollmasaż"],
+    mostPopular: false,
+  },
+  {
+    name: "Karnet Open",
+    id: "tier-open",
+    href: "#",
+    price: "149zł",
+    features: ["Sala ćwiczeń", "Sala cardio", "Zajęcia grupowe", "Rollmasaż"],
+    mostPopular: true,
+  },
+  {
+    name: "Karnet odnawialny",
+    id: "tier-renewal",
+    href: "#",
+    price: "119zł",
+    features: ["Sala ćwiczeń", "Sala cardio", "Zajęcia grupowe", "Rollmasaż"],
+    mostPopular: false,
+  },
+];
+
+export function Pricing() {
+  return (
+    <div className="grid max-w-md grid-cols-1 gap-8 mx-auto mt-16 isolate lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      {tiers.map((tier) => (
+        <div
+          key={tier.id}
+          className={cn(
+            tier.mostPopular
+              ? "ring-2 ring-lime-600 bg-lime-50"
+              : "ring-1 ring-gray-200 bg-gray-50",
+            "rounded-3xl p-8 xl:p-10"
+          )}
+        >
+          <div className="flex items-center justify-between gap-x-4">
+            <h3
+              id={tier.id}
+              className={cn(
+                tier.mostPopular ? "text-lime-600" : "text-gray-900",
+                "text-lg font-semibold leading-8"
+              )}
+            >
+              {tier.name}
+            </h3>
+            {tier.mostPopular ? (
+              <p className="rounded-full bg-lime-600/30 px-2.5 py-1 text-xs font-semibold leading-5 text-lime-700">
+                Najbardziej popularny
+              </p>
+            ) : null}
+          </div>
+          <p className="flex items-baseline mt-6 gap-x-1">
+            <span className="text-4xl font-bold tracking-tight text-gray-900">
+              {tier.price}
+            </span>
+          </p>
+          <ul
+            role="list"
+            className="mt-6 space-y-3 text-sm leading-6 text-gray-600"
+          >
+            {tier.features.map((feature) => (
+              <li key={feature} className="flex items-center gap-x-3">
+                <IconCircleCheck
+                  size={26}
+                  className="text-lime-600"
+                  aria-hidden="true"
+                />
+                <span className="text-lg tracking-tight text-bold">
+                  {feature}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
