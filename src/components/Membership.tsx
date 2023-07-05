2eb2f89839310565e46debb4fbf17c7aec25fe7a
Kacper Malek <kacper.malek0828@gmail.com>
2023-07-05 11:06:04 +0200

    add Membership component

---
+++ src/components/Membership.tsx
@@ -0,0 +1,15 @@
+import { Pricing } from "./Pricing";
+
+export function Membership() {
+  return (
+    <div id="membership" className="px-12 py-20 bg-lime-600/20">
+      <h3 className="text-4xl font-bold tracking-tight text-center">
+        Członkostwo
+      </h3>
+      <p className="mt-6 text-lg font-light tracking-tight text-center text-gray-200">
+        Nawiąż mocne więzi i dołącz do naszej fit społeczności!
+      </p>
+      <Pricing />
+    </div>
+  );
+}
