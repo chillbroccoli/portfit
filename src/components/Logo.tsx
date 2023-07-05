81af414b2d18db726dff1a9845dc8250bdfa8fa9
Kacper Malek <kacper.malek0828@gmail.com>
2023-07-05 11:05:58 +0200

    add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export function Logo() {
+  return (
+    <div className="relative overflow-hidden rounded-md">
+      <p className="text-4xl font-bold tracking-tight text-white uppercase">
+        Port<span className="text-lime-600">Fit</span>
+      </p>
+    </div>
+  );
+}
