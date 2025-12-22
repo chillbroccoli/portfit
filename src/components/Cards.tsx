8894cf7b8a551ecbe3975a143ab5b6ef6185bcc5
Kacper Malek <kacper.malek0828@gmail.com>
2025-12-22 14:51:34 +0100

    Update grid layout and image alignment in Cards component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -13,7 +13,7 @@ export function Cards() {
         <h2 className="text-2xl font-bold tracking-tight text-center text-gray-100">
           Obsługujemy karty
         </h2>
-        <div className="grid items-center max-w-xl grid-cols-3 mx-auto mt-4 gap-x-4 gap-y-10 sm:grid-cols-6 lg:grid-cols-4">
+        <div className="grid items-center max-w-xl grid-cols-1 mx-auto mt-4 gap-x-4 gap-y-6 lg:grid-cols-4">
           {imgs.map((img, index) => (
             <Image
               key={index}
@@ -21,7 +21,7 @@ export function Cards() {
               alt=""
               width={158}
               height={72}
-              className="col-span-2 max-h-14 lg:max-h-16 lg:col-span-1"
+              className="max-h-14 lg:max-h-16 mx-auto"
             />
           ))}
         </div>
